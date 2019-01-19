2673eb415e38c0ded44c1990b40867d67962872f
Vo Thai Loc <lasttt1111@gmail.com>
2019-01-20 03:32:15 +0700

    submit report

--- app/Models/System/Submission.php
+++ app/Models/System/Submission.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Submission extends Model
 {
+	public $timestamps = false;
+
     protected $table = 'submission';
 
-    protected $fillable = ['user_id', 'address', 'subt_date', 'reason', 'approved', 'appr_user', 'appr_date'];
+    protected $fillable = ['id', 'user_id', 'address', 'subt_date', 'reason', 'approved', 'appr_user', 'appr_date'];
 
 }
