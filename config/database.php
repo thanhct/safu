c050d56d48689337a4db96ce7360cd48e153a661
tuyhpq <tuyhpq@gmail.com>
2019-01-20 00:55:11 +0700

    add new database

--- config/database.php
+++ config/database.php
@@ -48,11 +48,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sg4.fcomet.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'fbvnorg_safu'),
+            'username' => env('DB_USERNAME', 'fbvnorg_safu'),
+            'password' => env('DB_PASSWORD', 'safu123'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
