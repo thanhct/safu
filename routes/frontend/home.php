0dd3c7d3bb87beeaa5e28e0f10e50ed1ef80291e
AnhTai1995 <anhtai.hcmus95@gmail.com>
2019-01-20 09:18:49 +0700

    Add middleware check auth in route index home

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Frontend\User\DashboardController;
  * Frontend Controllers
  * All route names are prefixed with 'frontend.'.
  */
-Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/', [HomeController::class, 'index'])->name('index')->middleware('auth');
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
 Route::post('contact/send', [ContactController::class, 'send'])->name('contact.send');
 
